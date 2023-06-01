8889e09131dfb8792d91f0f7faee12b29e6068b8
Vinicius  de Souza Rezende <06812485560@academico.fja.edu.br>
2023-06-01 17:22:29 -0300

    Iniciando o jogo da forca.

--- Técnicas e Linguagens de Programação/Python/Exercícios/03 - Forca/main.py
+++ Técnicas e Linguagens de Programação/Python/Exercícios/03 - Forca/main.py
@@ -1,11 +1,33 @@
 from random import choice
+from os import system
 
 escolhas = ['PALAVRA', 'OUTRA', 'COMPUTADOR', 'NOVAMENTE', 'FACULDADE']
 
 palavraEscolhida = choice(escolhas)
-
 palavraEscolhidaModificada = list('-'*len(palavraEscolhida))
 
-print(palavraEscolhidaModificada)
+def iniciaJogo():
 
+    
+
+
+
+
+
+
+
+    input("Aperte ENTER para continuar!")
+
+while True:
+    system('cls')
+    opcao = input(f"Digite uma das seguintes opções:\n[1] - Nova tentativa;\n[2] - Sair\nPalavra: {palavraEscolhidaModificada}\nOpção: ")
+
+    if opcao == '1':
+        iniciaJogo()
+    elif opcao == '2':
+        break
+    else:
+        print("Opção inválida!")
+        input("Aperte ENTER para continuar!")
+    
 
